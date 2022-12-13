675a99a923e310276961bad99454b23142fe6a6c
Johnnytega <oghenetegajohnny16@gmail.com>
2022-12-13 15:18:58 -0800

    2. 10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * Print_alphabet_x10 - a function that prints 10 times the alphabet
+ * print_alphabet_x10 - a function that prints 10 times the alphabet
  *
  */
 void print_alphabet_x10(void)
